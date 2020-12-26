712b8379dab454a5c8d5c876cc3df69373c6a24e
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-26 15:05:54 -0600

    script creating XML

--- script.py
+++ script.py
@@ -1,9 +1,34 @@
 ###--- IMPORTS ---###
+import xml.etree.ElementTree as ET
+'''
+Warning The xml.etree.ElementTree 
+module is not secure against maliciously constructed data. 
+If you need to parse untrusted or 
+unauthenticated data see XML vulnerabilities.
+(https://docs.python.org/3/library/xml.etree.elementtree.html#module-xml.etree.ElementTree)
+'''
+
+###--- CODE ---###
 
 
-###--- IMPORTS ---###
+def make_xml():
+    '''
+    Creates 'root' element with 'xml.etree.ElementTree' object,
+    then creates 'product' subelement, and its attributes and elements.
+    Finaly, writes the element tree to a file as XML.
+    '''
+    xml_doc = ET.Element('root')
+
+    product = ET.SubElement(xml_doc, 'product')
+
+    ET.SubElement(product, 'item', id='product_id', price='9.99',
+                  currency='USD').text = "Product description in detail since attributes already included."
+    ET.SubElement(product, 'brand', model='testtesttest123').text = "Brand XYZ"
+
+    tree = ET.ElementTree(xml_doc)
+    tree.write('output_test.xml', encoding='UTF-8', xml_declaration=True)
 
 
-###--- IMPORTS ---###
+###--- DRIVER CODE ---###
 if __name__ == "__main__":
-    pass
+    make_xml()
